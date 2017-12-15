e4d574aa90e800616da625aa2a590d11a8cc6970
unknown <dws@yoyoma.com>
2017-12-15 13:51:04 -0500

    Converted test to the fixture type.

--- tests/Matrix_Test.cpp
+++ tests/Matrix_Test.cpp
@@ -14,22 +14,34 @@
 using ::testing::ElementsAre;
 using ::testing::ContainerEq;
 
-TEST(MatrixTest, AddRowsValid){
-	Matrix myMatrix(3, 4);
+class MatrixTest : public ::testing::Test {
+	protected:
+		Matrix * matrixThreeByFour;
 
-	int row1Arr[] = {1, 2, 3, 4};
-	int row2Arr[] = {5, 6, 7, 8};
-	int row3Arr[] = {9, 10, 11, 12};
-	std::vector<int> row1Vect(row1Arr, row1Arr + (sizeof(row1Arr)/sizeof(row1Arr[0])));
-	std::vector<int> row2Vect(row2Arr, row2Arr + (sizeof(row2Arr)/sizeof(row2Arr[0])));
-	std::vector<int> row3Vect(row3Arr, row3Arr + (sizeof(row3Arr)/sizeof(row3Arr[0])));
+		virtual void SetUp() {
+			matrixThreeByFour = new Matrix(3, 4);
 
-	myMatrix.addRow(row1Vect);
-	myMatrix.addRow(row2Vect);
-	myMatrix.addRow(row3Vect);
+			int row1Arr[] = {1, 2, 3, 4};
+			int row2Arr[] = {5, 6, 7, 8};
+			int row3Arr[] = {9, 10, 11, 12};
+			std::vector<int> row1Vect(row1Arr, row1Arr + (sizeof(row1Arr)/sizeof(row1Arr[0])));
+			std::vector<int> row2Vect(row2Arr, row2Arr + (sizeof(row2Arr)/sizeof(row2Arr[0])));
+			std::vector<int> row3Vect(row3Arr, row3Arr + (sizeof(row3Arr)/sizeof(row3Arr[0])));
 
+			matrixThreeByFour->addRow(row1Vect);
+			matrixThreeByFour->addRow(row2Vect);
+			matrixThreeByFour->addRow(row3Vect);
+		}
 
-	EXPECT_THAT(myMatrix[0], ElementsAre(1, 2, 3, 4));
+		virtual void TearDown() {
+			delete matrixThreeByFour;
+		}
+};
+
+TEST_F(MatrixTest, AddRowsValid){
+	EXPECT_THAT((*matrixThreeByFour)[0], ElementsAre(1, 2, 3, 4));
+	EXPECT_THAT((*matrixThreeByFour)[1], ElementsAre(5, 6, 7, 8));
+	EXPECT_THAT((*matrixThreeByFour)[2], ElementsAre(9, 10, 11, 12));
 }
 
 
