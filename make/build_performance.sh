2c70af65163785507d68149b510f47f24746967d
unknown <dws@yoyoma.com>
2017-12-20 15:12:12 -0500

    Modified build_performance script to create separate performance analysis for individual tests.  Also added more performance tests.

--- make/build_performance.sh
+++ make/build_performance.sh
@@ -1,6 +1,24 @@
 set -x
+
 make performance
-mkdir perf_analysis 
-CodeXLCpuProfiler.exe collect -m tbp -o perf_analysis/perf EnterTheMatrix_Perf_Test.exe
-CodeXLCpuProfiler.exe report -i perf_analysis/perf.prd -I -o perf_analysis/
-python3 /c/Development/CPP/CsvMaker/src/CodeXlCpuProfilerExtractor.py -i perf_analysis/perf/perf.csv -o perf_analysis/perf/duration.csv
+
+mkdir -p perf_analysis/MatrixPerfTest
+
+path_BigMultiply='perf_analysis/MatrixPerfTest/BigMultiply'
+path_BiggerMultiply='perf_analysis/MatrixPerfTest/BiggerMultiply'
+path_BiggestMultiply='perf_analysis/MatrixPerfTest/BiggestMultiply'
+mkdir $path_BigMultiply
+mkdir $path_BiggerMultiply
+mkdir $path_BiggestMultiply
+
+CodeXLCpuProfiler.exe collect -m tbp -o $path_BigMultiply/perf_BigMultiply EnterTheMatrix_Perf_Test.exe --gtest_filter=MatrixPerfTest.BigMultiply
+CodeXLCpuProfiler.exe report -i $path_BigMultiply/perf_BigMultiply.prd -I -o $path_BigMultiply
+python3 /c/Development/CPP/CsvMaker/src/CodeXlCpuProfilerExtractor.py -i $path_BigMultiply/perf_BigMultiply/perf_BigMultiply.csv -o $path_BigMultiply/perf_BigMultiply/duration_BigMultiply.csv
+
+CodeXLCpuProfiler.exe collect -m tbp -o $path_BiggerMultiply/perf_BiggerMultiply EnterTheMatrix_Perf_Test.exe --gtest_filter=MatrixPerfTest.BiggerMultiply
+CodeXLCpuProfiler.exe report -i $path_BiggerMultiply/perf_BiggerMultiply.prd -I -o $path_BiggerMultiply
+python3 /c/Development/CPP/CsvMaker/src/CodeXlCpuProfilerExtractor.py -i $path_BiggerMultiply/perf_BiggerMultiply/perf_BiggerMultiply.csv -o $path_BiggerMultiply/perf_BiggerMultiply/duration_BiggerMultiply.csv
+
+CodeXLCpuProfiler.exe collect -m tbp -o $path_BiggestMultiply/perf_BiggestMultiply EnterTheMatrix_Perf_Test.exe --gtest_filter=MatrixPerfTest.BiggestMultiply
+CodeXLCpuProfiler.exe report -i $path_BiggestMultiply/perf_BiggestMultiply.prd -I -o $path_BiggestMultiply
+python3 /c/Development/CPP/CsvMaker/src/CodeXlCpuProfilerExtractor.py -i $path_BiggestMultiply/perf_BiggestMultiply/perf_BiggestMultiply.csv -o $path_BiggestMultiply/perf_BiggestMultiply/duration_BiggestMultiply.csv
--- tests/Matrix_Perf_Test.cpp
+++ tests/Matrix_Perf_Test.cpp
@@ -11,8 +11,20 @@
 
 // Multiplication of large matrices.
 TEST(MatrixPerfTest, BigMultiply) {
+	Matrix matrix1(3000, 3000, 1000);
+	Matrix matrix2(3000, 3000, 9900);
+	Matrix matrix3 = matrix1 * matrix2;
+}
+
+TEST(MatrixPerfTest, BiggerMultiply) {
 	Matrix matrix1(5000, 5000, 1000);
 	Matrix matrix2(5000, 5000, 9900);
+	Matrix matrix3 = matrix1 * matrix2;
+}
+
+TEST(MatrixPerfTest, BiggestMultiply) {
+	Matrix matrix1(7000, 7000, 1000);
+	Matrix matrix2(7000, 7000, 9900);
 	Matrix matrix3 = matrix1 * matrix2;
 }
 
