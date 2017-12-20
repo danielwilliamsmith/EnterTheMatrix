7c9e582e30e103a027b31cfa0ba55b8a3cbf0688
unknown <dws@yoyoma.com>
2017-12-20 18:02:11 -0500

    The build_performance script now summarizes all results in a single csv file

--- make/build_performance.sh
+++ make/build_performance.sh
@@ -2,23 +2,22 @@ set -x
 
 make performance
 
-mkdir -p perf_analysis/MatrixPerfTest
+path_root='perf_analysis/MatrixPerfTest' 
+mkdir -p $path_root
 
-path_BigMultiply='perf_analysis/MatrixPerfTest/BigMultiply'
-path_BiggerMultiply='perf_analysis/MatrixPerfTest/BiggerMultiply'
-path_BiggestMultiply='perf_analysis/MatrixPerfTest/BiggestMultiply'
-mkdir $path_BigMultiply
-mkdir $path_BiggerMultiply
-mkdir $path_BiggestMultiply
+path_all_csv=$path_root'/all_csv'
+mkdir $path_all_csv
 
-CodeXLCpuProfiler.exe collect -m tbp -o $path_BigMultiply/perf_BigMultiply EnterTheMatrix_Perf_Test.exe --gtest_filter=MatrixPerfTest.BigMultiply
-CodeXLCpuProfiler.exe report -i $path_BigMultiply/perf_BigMultiply.prd -I -o $path_BigMultiply
-python3 /c/Development/CPP/CsvMaker/src/CodeXlCpuProfilerExtractor.py -i $path_BigMultiply/perf_BigMultiply/perf_BigMultiply.csv -o $path_BigMultiply/perf_BigMultiply/duration_BigMultiply.csv
+CodeXLCpuProfiler.exe collect -m tbp -o $path_root/BigMultiply EnterTheMatrix_Perf_Test.exe --gtest_filter=MatrixPerfTest.BigMultiply
+CodeXLCpuProfiler.exe report -i $path_root/BigMultiply.prd -I -o $path_root
+cp $path_root/BigMultiply/BigMultiply.csv $path_all_csv
 
-CodeXLCpuProfiler.exe collect -m tbp -o $path_BiggerMultiply/perf_BiggerMultiply EnterTheMatrix_Perf_Test.exe --gtest_filter=MatrixPerfTest.BiggerMultiply
-CodeXLCpuProfiler.exe report -i $path_BiggerMultiply/perf_BiggerMultiply.prd -I -o $path_BiggerMultiply
-python3 /c/Development/CPP/CsvMaker/src/CodeXlCpuProfilerExtractor.py -i $path_BiggerMultiply/perf_BiggerMultiply/perf_BiggerMultiply.csv -o $path_BiggerMultiply/perf_BiggerMultiply/duration_BiggerMultiply.csv
+CodeXLCpuProfiler.exe collect -m tbp -o $path_root/BiggerMultiply EnterTheMatrix_Perf_Test.exe --gtest_filter=MatrixPerfTest.BiggerMultiply
+CodeXLCpuProfiler.exe report -i $path_root/BiggerMultiply.prd -I -o $path_root
+cp $path_root/BiggerMultiply/BiggerMultiply.csv $path_all_csv
 
-CodeXLCpuProfiler.exe collect -m tbp -o $path_BiggestMultiply/perf_BiggestMultiply EnterTheMatrix_Perf_Test.exe --gtest_filter=MatrixPerfTest.BiggestMultiply
-CodeXLCpuProfiler.exe report -i $path_BiggestMultiply/perf_BiggestMultiply.prd -I -o $path_BiggestMultiply
-python3 /c/Development/CPP/CsvMaker/src/CodeXlCpuProfilerExtractor.py -i $path_BiggestMultiply/perf_BiggestMultiply/perf_BiggestMultiply.csv -o $path_BiggestMultiply/perf_BiggestMultiply/duration_BiggestMultiply.csv
+CodeXLCpuProfiler.exe collect -m tbp -o $path_root/BiggestMultiply EnterTheMatrix_Perf_Test.exe --gtest_filter=MatrixPerfTest.BiggestMultiply
+CodeXLCpuProfiler.exe report -i $path_root/BiggestMultiply.prd -I -o $path_root
+cp $path_root/BiggestMultiply/BiggestMultiply.csv $path_all_csv
+
+python3 /c/Development/CPP/CsvMaker/src/CodeXlCpuProfilerExtractor.py -i $path_all_csv -o $path_all_csv/duration.csv
