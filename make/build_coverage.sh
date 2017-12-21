acbb7d1e6e17c4dbc4f0c6f47bce19af4c553e6e
unknown <dws@yoyoma.com>
2017-12-21 18:08:33 -0500

    Updated build_coverage script to include LCOV.

--- make/build_coverage.sh
+++ make/build_coverage.sh
@@ -2,4 +2,8 @@ set -x
 make coverage
 ./EnterTheMatrix_Test.exe --gtest_output=xml:output.xml
 mkdir cov_analysis
-gcovr ./ -r /c/msys64/var/jenkins/workspace/EnterTheMatrix/src --gcov-exclude='.*_Test.*' -x -o cov_analysis/coverage.xml
+mkdir cov_analysis/gcov
+mkdir cov_analysis/lcov
+gcovr ./ -r /c/msys64/var/jenkins/workspace/EnterTheMatrix/src --gcov-exclude='.*_Test.*' -x -o cov_analysis/gcov/coverage.xml
+lcov -c -d ./ -d /c/msys64/var/jenkins/workspace/EnterTheMatrix/src --no-external --rc lcov_branch_coverage=1 -o cov_analysis/lcov/coverage.info
+genhtml cov_analysis/lcov/coverage.info --rc genhtml_branch_coverage=1 -o cov_analysis/lcov/coverage_html
