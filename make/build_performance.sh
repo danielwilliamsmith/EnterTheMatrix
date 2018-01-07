0cadee14cb8037de031b1dcfc422f6bbfcfc25f8
unknown <dws@yoyoma.com>
2018-01-07 13:01:12 -0500

    Enabled the performance tests.

--- make/build_performance.sh
+++ make/build_performance.sh
@@ -12,12 +12,12 @@ CodeXLCpuProfiler.exe collect -m tbp -o $path_root/BigMultiply EnterTheMatrix_Pe
 CodeXLCpuProfiler.exe report -i $path_root/BigMultiply.prd -I -o $path_root
 cp $path_root/BigMultiply/BigMultiply.csv $path_all_csv
 
-#CodeXLCpuProfiler.exe collect -m tbp -o $path_root/BiggerMultiply EnterTheMatrix_Perf_Test.exe --gtest_filter=MatrixPerfTest.BiggerMultiply
-#CodeXLCpuProfiler.exe report -i $path_root/BiggerMultiply.prd -I -o $path_root
-#cp $path_root/BiggerMultiply/BiggerMultiply.csv $path_all_csv
+CodeXLCpuProfiler.exe collect -m tbp -o $path_root/BiggerMultiply EnterTheMatrix_Perf_Test.exe --gtest_filter=MatrixPerfTest.BiggerMultiply
+CodeXLCpuProfiler.exe report -i $path_root/BiggerMultiply.prd -I -o $path_root
+cp $path_root/BiggerMultiply/BiggerMultiply.csv $path_all_csv
 
-#CodeXLCpuProfiler.exe collect -m tbp -o $path_root/BiggestMultiply EnterTheMatrix_Perf_Test.exe --gtest_filter=MatrixPerfTest.BiggestMultiply
-#CodeXLCpuProfiler.exe report -i $path_root/BiggestMultiply.prd -I -o $path_root
-#cp $path_root/BiggestMultiply/BiggestMultiply.csv $path_all_csv
+CodeXLCpuProfiler.exe collect -m tbp -o $path_root/BiggestMultiply EnterTheMatrix_Perf_Test.exe --gtest_filter=MatrixPerfTest.BiggestMultiply
+CodeXLCpuProfiler.exe report -i $path_root/BiggestMultiply.prd -I -o $path_root
+cp $path_root/BiggestMultiply/BiggestMultiply.csv $path_all_csv
 
 python3 /c/Development/CPP/CsvMaker/src/CodeXlCpuProfilerExtractor.py -i $path_all_csv -o $path_all_csv/duration.csv
